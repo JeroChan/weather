ca59d1ec367030b97776f0fe8c29570352ce2d35
JeroChan <446363041@qq.com>
2019-10-12 08:26:20 +0000

    Apply fixes from StyleCI

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
 
+/*
+ * This file is part of the overtrue/weather.
+ *
+ * (c) jerochan <446363041@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 require './src/Weather.php';
 require './src/Exceptions/Exception.php';
 require './src/Exceptions/InvalidArgumentException.php';
-
 
 $w = new \Jero\Weather\Weather('f46c94283419ed0dbb4e7e2f6bc4041b');
 
